8527db9d4078baff3de3de694f7e8b02f3f37cd3
lfsmoura <leofsmoura@gmail.com>
2016-05-15 19:21:05 -0300

    Sort ranking

--- src/assets/js/Ranking.js
+++ src/assets/js/Ranking.js
@@ -8,7 +8,11 @@ export default class Ranking extends React.Component {
   }
 
   render() {
-    var userList = this.props.users.map((user) => {
+    const users = this.props.users.sort(function(userA, userB) {
+        return userA.displayName.length - userB.displayName.length;
+      });
+    console.log(users);
+    var userList = users.map((user) => {
       return (<li key={`player-${user.id}`} className="list-group-item">
           <UserBox user={user}/>
         </li>);
