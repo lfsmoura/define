59abff90992c3ff0475624f45a48c634da871017
lfsmoura <leofsmoura@gmail.com>
2016-05-15 13:42:49 -0300

    Remove react router

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,18 +2,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router'
-
 import Index from './Index.js';
-import UserBox from './UserBox.js';
 
 import { defineStore } from './DefineStore.js';
 import { socket } from './Game.js';
 
 const render = () => {
-  ReactDOM.render((<Router history={hashHistory}>
-    <Route path="/" component={Index}/>
-  </Router>),
+  ReactDOM.render(<Index/>,
       document.getElementById('main'));
 };
 
